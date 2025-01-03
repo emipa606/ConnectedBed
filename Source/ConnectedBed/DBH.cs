7eb08052f1c5d641616c5c0a92ccdf3852454619
Mlie <emipa606@users.noreply.github.com>
2025-01-03 18:55:39 +0100

    Fixed debug-menu issue when DBH isnt loaded

--- Source/ConnectedBed/DBH.cs
+++ Source/ConnectedBed/DBH.cs
@@ -15,30 +15,19 @@ public class DBH
             return;
         }
 
-        checkThirst(pawn, pipeComp);
-        checkBladder(pawn, pipeComp);
-    }
-
-    private static void checkBladder(Pawn pawn, CompPipe pipeComp)
-    {
         var bladder = pawn.needs.TryGetNeed<Need_Bladder>();
-        if (bladder == null || bladder.CurLevelPercentage >= 0.5)
+        if (bladder != null && !(bladder.CurLevelPercentage >= 0.5))
         {
-            return;
+            var urineAmount = (1.0f - bladder.CurLevelPercentage) * waterPerDay;
+            if (pipeComp.pipeNet.PushSewage(urineAmount * 2))
+            {
+                bladder.dump();
+                bladder.CurLevel = 1;
+            }
+
+            pipeComp.pipeNet.PullWater(urineAmount, out _); // flush
         }
 
-        var urineAmount = (1.0f - bladder.CurLevelPercentage) * waterPerDay;
-        if (pipeComp.pipeNet.PushSewage(urineAmount * 2))
-        {
-            bladder.dump();
-            bladder.CurLevel = 1;
-        }
-
-        pipeComp.pipeNet.PullWater(urineAmount, out _); // flush
-    }
-
-    private static void checkThirst(Pawn pawn, CompPipe pipeComp)
-    {
         var thirst = pawn.needs.TryGetNeed<Need_Thirst>();
         if (thirst == null || thirst.CurLevelPercentage >= 0.5)
         {
